a700ab3e19698f8ec8cb46f3f9758c5680db1ce2
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-09-05 08:43:34 -0300

    fix: pagina inicial

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout() {
       <AuthProvider>
         <AppContextProvider>
           <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
             <Stack.Screen name="(auth)" options={{ headerShown: false }} />
             <Stack.Screen name="+not-found" />
